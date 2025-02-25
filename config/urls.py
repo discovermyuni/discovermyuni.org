4bf87a132972ff3b6cfd08bbd59a87b8d221501c
corgi-in-tights <59304120+corgi-in-tights@users.noreply.github.com>
2025-02-25 13:43:49 -0500

    added about and home templates to root urls.py

--- config/urls.py
+++ config/urls.py
@@ -1,12 +1,14 @@
 from django.contrib import admin
 from django.urls import include
 from django.urls import path
+from django.views.generic import TemplateView
 
 urlpatterns = [
+    path("", TemplateView.as_view(template_name="pages/home.html"), name="home"),
     path("", include("posts.urls")),
+    path("about/", TemplateView.as_view(template_name="pages/about.html"), name="about"),
     path("admin/", admin.site.urls),
     path("accounts/", include("allauth.urls")),
     path("users/", include("users.urls")),
-
     path("api/posts/", include("posts.api_urls")),
 ]
